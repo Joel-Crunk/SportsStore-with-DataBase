985ad03f75712ddb9a8eb22832562f4f60358e92
Joel-Crunk <joel.crunk@outlook.com>
2019-05-17 18:07:40 -0400

    the Shopping Cart

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -22,15 +22,15 @@ namespace SportsStore.UnitTests
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
             mock.Setup(m => m.Products).Returns(new Product[] {
                 new Product {ProductID = 1, Name = "P1"},
-new Product {ProductID = 2, Name = "P2"},
-new Product {ProductID = 3, Name = "P3"},
-new Product {ProductID = 4, Name = "P4"},
-new Product {ProductID = 5, Name = "P5"}
+                 new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                 new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
 });
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
             // Act
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null, 2).Model;
             // Assert
             Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
@@ -76,7 +76,7 @@ new Product {ProductID = 5, Name = "P5"}
         ProductController controller = new ProductController(mock.Object);
         controller.PageSize = 3;
         // Act
-        ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+        ProductsListViewModel result = (ProductsListViewModel)controller.List(null, 2).Model;
         // Assert
         PagingInfo pageInfo = result.PagingInfo;
         Assert.AreEqual(pageInfo.CurrentPage, 2);
